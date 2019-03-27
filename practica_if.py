decc46514ea9de919870bb3d0f48f7765b9952da
jose <josepicoas@gmail.com>
2019-03-27 10:10:01 -0300

    subo mas ejemplos de condicionales

--- practica_if.py
+++ practica_if.py
@@ -11,3 +11,27 @@ def evaluacion(note):
 
 print(evaluacion(int(nota_alumno)))  #casteo la entrada a int
 
+
+# if-else
+edad = int(input("Introduce tu edad"))
+
+if edad >= 18:
+	print("Puedes pasar")
+else:
+	print("No pudes pasar")
+
+print("Fin")
+
+# elif
+
+edad2 = int(input("Introduce tu edad"))
+
+if edad2 < 18:
+	print("No Puedes Pasar")
+elif edad2 < 21:
+	print("Queda a tu criterio")
+else:
+	print("Ok puedes pasar!")
+
+print("Fin")
+
