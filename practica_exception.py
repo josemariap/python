01b2e64251151c57c457b84fd38914bdf06d45c8
jose pico <josepico@MacBook-Pro-de-jose.local>
2019-04-22 20:43:46 -0300

    subo practica de exception

---
+++ practica_exception.py
@@ -0,0 +1,43 @@
+# exceptions
+
+def suma(num1, num2):
+	return num1+num2
+
+def resta(num1, num2):
+	return num1-num2
+
+def multiplica(num1, num2):
+	return num1*num2
+
+def divide(num1,num2):
+	try:
+		return num1/num2
+	except ZeroDivisionError:
+	 	print("No esta permitido la division por 0")
+	 	return "Error en la operación"
+
+	
+
+op1=(int(input("Introduce el primer número: ")))
+
+op2=(int(input("Introduce el segundo número: ")))		
+	
+operacion=input("Introduce la operación a realizar (suma,resta,multiplica,divide): ")
+
+if operacion=="suma":
+	print(suma(op1,op2))
+
+elif operacion=="resta":
+	print(resta(op1,op2))
+
+elif operacion=="multiplica":
+	print(multiplica(op1,op2))
+
+elif operacion=="divide":
+	print(divide(op1,op2))
+
+else:
+	print ("Operación no contemplada")
+
+print("Continua la ejecución del programa..")
+
