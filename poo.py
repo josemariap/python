a065a6915ee0ec2776e10eb4065ad9d993dad555
josemariap <josepicoas@gmail.com>
2019-05-02 10:34:38 -0300

    constructor y encapsulacion

--- poo.py
+++ poo.py
@@ -1,26 +1,48 @@
 # clases
 
 class Coche():
-    largochasis=250
-    anchoChasis=4
-    ruedas=4
-    enmarcha=False
 
-    def arrancar(self):  #self = this
-        self.enmarcha=True
-        
+    #Constructor. defino los atributos de la calse directamente en el constructor
+    def __init__(self):
+    	 self.largochasis=255
+    	 self.anchoChasis=4
+    	 self.__ruedas=4    #doble guion sirve para encapsular o hacer private el atributo/variable
+    	 self.enmarcha=False
+
+    def arrancar(self, arrancamos):  #self = this
+        self.enmarcha=arrancamos
+        if(self.enmarcha):
+        	return "El auto esta en marcha"
+        else:
+        	return "El auto esta detenido"
+
+  
     def estado(self):
         if(self.enmarcha):
             return "El auto esta en marcha"
         else:
             return "El auto esta parado"
 
+    def canTRuedas(self):
+        return self.__ruedas #cuando quiero acceder al atributo privado uso tambien doble guion junto el col self
+
+
+
+#################################################
+
 miCoche=Coche()  #instancia de clase
 
 print("El largo de mi coche es :", miCoche.largochasis)
-print("El coche tiene :", miCoche.ruedas, " ruedas")
+print("El coche tiene :", miCoche.canTRuedas(), " ruedas") #cuando quiero acceder al atributo privado uso tambien doble guion y seria como un geter
 
-miCoche.arrancar()
+print(miCoche.arrancar(False))
 
 print("El auto esta en marcha", miCoche.enmarcha)
 print(miCoche.estado())
+
+print("Segundo objeto")
+
+miCoche2=Coche()
+miCoche2.arrancar(True)
+print(miCoche2.estado())
+
