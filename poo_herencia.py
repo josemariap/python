651dcc831501635c7008b1571b6085fd01fab60f
josemariap <josepicoas@gmail.com>
2019-05-06 16:50:12 -0300

    herencia

--- poo_herencia.py
+++ poo_herencia.py
@@ -21,14 +21,45 @@ class Vehiculo():
         print("Marca: ", self.marca, "Modelo: ", self.modelo, "En marcha: ", self.enmarca, "Acelerando: ", self.acelera, "Frenando: ", self.frena)
 
 
+# La clase Furgoneta hereda de Vehiculo
+class Furgoneta(Vehiculo):
+    cargado=False
+
+    def carga(self, cargar):
+        self.cargado=cargar
+    
+    #overrite del metodo estado()
+    def estado(self):
+        print("Marca: ", self.marca, "Modelo: ", self.modelo, "En marcha: ", self.enmarca, "Acelerando: ", self.acelera, "Frenando: ", self.frena, "Cargada: ", self.cargado)
+
+
+
+
 # La clase Moto hereda de Vehiculo
-
 class Moto(Vehiculo):
-    pass
+    hcaballito=""
 
+    def caballito(self):
+        self.hcaballito="Voy haciendo caballito"
+    
+    #overrite del metodo estado()
+    def estado(self):
+        print("Marca: ", self.marca, "Modelo: ", self.modelo, "En marcha: ", self.enmarca, "Acelerando: ", self.acelera, "Frenando: ", self.frena, "Caballito: ", self.hcaballito)
+
+
+
+
+############################################
 
 miMoto=Moto("Honda", "cbr")
 
+miMoto.caballito()
 miMoto.estado()
 
+miCamioneta=Furgoneta("Chevrolet", "C10")
+miCamioneta.carga(True)
+miCamioneta.estado()
 
+# puedo tener multiple herencia
+class MotoDeCarga(Moto, Furgoneta):  #hereda de Moto y de Furgoneta
+    pass
